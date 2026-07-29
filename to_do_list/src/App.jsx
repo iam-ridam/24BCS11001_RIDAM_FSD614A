2b578aa62602a7a08c596170df01194df1b3049b
ridam <bhatiaridam1@gmail.com>
2026-07-29 22:29:47 +0530

    add todo

---
+++ to_do_list/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Todo from './Todo.jsx'
+
+function App() {
+
+  return (
+    <Todo/>
+  )
+}
+
+export default App
---
+++ to_do_list/src/Todo.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function Todo() {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if (task.trim() === "") return;
+
+    setTodos([...todos, task]);
+    setTask("");
+  };
+
+  const deleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+
+      <input
+        type="text"
+        placeholder="Enter task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button onClick={addTodo}>Add</button>
+
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => deleteTodo(index)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Todo;
