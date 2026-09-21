5faea330ef1173529d9bea02cf40e743a364cd4b
ridam <bhatiaridam1@gmail.com>
2026-09-21 09:20:20 +0530

    add node

--- node/index.js
+++ node/index.js
@@ -1,4 +1,4 @@
-// server.mjs
+
 import { createServer } from 'node:http';
 
 const server = createServer((req, res) => {
@@ -6,9 +6,8 @@ const server = createServer((req, res) => {
   res.end('Hello World!\n');
 });
 
-// starts a simple http server locally on port 3000
+
 server.listen(3000, '127.0.0.1', () => {
   console.log('Listening on 127.0.0.1:3000');
 });
 
-// run with `node server.mjs`
